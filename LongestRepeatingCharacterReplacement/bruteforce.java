62d91d292727cea685927ccf6b48c2218b8e7482
Deepak <dbohara88@gmail.com>
2024-11-08 11:28:38 +0530

    mac

--- LongestRepeatingCharacterReplacement/bruteforce.java
+++ LongestRepeatingCharacterReplacement/bruteforce.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 public class bruteforce {
     public int characterReplacement(String s, int k) {
-        
+        int[] charsCount  = new int[26];
+        int i = 0;
+        int maxCount = 0;
+        int ans = 0;
+
+        for(int j=0; j<s.length(); j++) {
+            charsCount[s.charAt(j)-'A']++;
+            maxCount = Math.max(maxCount, charsCount[s.charAt(j)-'A']);
+
+            while(j-i+1-maxCount > k) {
+                charsCount[s.charAt(i)-'A']--;
+                i++;
+            }
+            ans = Math.max(ans, j-i+1);
+        }
+        return ans;
     }
     public static void main(String[] args) {
         Scanner ss = new Scanner(System.in);
